0fe6815b92e8665448851441593b3a62f6d40c17
Arror <maqiang@autoai.com>
2022-03-24 21:03:52 +0800

    Using time line view

--- iOS/Home/VPNConnecteDurationView.swift
+++ iOS/Home/VPNConnecteDurationView.swift
@@ -3,38 +3,29 @@ import SwiftUI
 struct VPNConnecteDurationView: View {
     
     @EnvironmentObject private var controller: VPNController
-    
-    @State private var connectedDuration: String = ""
-    
+        
     var body: some View {
-        HStack {
-            Image(systemName: "clock")
-                .font(.title2)
-                .foregroundColor(Color.accentColor)
-            Text("连接时间")
-            Spacer()
-            Text(connectedDuration)
-                .foregroundColor(.secondary)
-        }
-        .onReceive(Timer.publish(every: 0.5, on: .current, in: .common).autoconnect()) { _ in
-            guard let date = controller.connectedDate else {
-                return connectedDuration = ""
-            }
-            let duration = Int64(abs(date.timeIntervalSinceNow))
-            let hs = duration / 3600
-            let ms = duration % 3600 / 60
-            let ss = duration % 60
-            connectedDuration = String(format: "%02d:%02d:%02d", hs, ms, ss)
-        }
-        .onChange(of: controller.connectionStatus) { status in
-            switch status {
-            case .invalid, .connecting, .disconnected:
-                connectedDuration = ""
-            case .connected, .disconnecting, .reasserting:
-                break
-            @unknown default:
-                connectedDuration = ""
+        TimelineView(.periodic(from: Date(), by: 1.0)) { context in
+            HStack {
+                Image(systemName: "clock")
+                    .font(.title2)
+                    .foregroundColor(Color.accentColor)
+                Text("连接时间")
+                Spacer()
+                Text(durationFormatString(current: context.date))
+                    .foregroundColor(.secondary)
             }
         }
+    }
+    
+    private func durationFormatString(current: Date) -> String {
+        guard let date = controller.connectedDate else {
+            return ""
+        }
+        let duration = Int64(abs(date.distance(to: current)))
+        let hs = duration / 3600
+        let ms = duration % 3600 / 60
+        let ss = duration % 60
+        return String(format: "%02d:%02d:%02d", hs, ms, ss)
     }
 }
