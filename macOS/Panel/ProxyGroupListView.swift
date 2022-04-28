31b323f303bc01bde91a8c60d47a5b2d4e89345a
Arror <maqiang@autoai.com>
2022-04-28 15:08:44 +0800

    Using group box

--- macOS/Panel/ProxyGroupListView.swift
+++ macOS/Panel/ProxyGroupListView.swift
@@ -19,27 +19,25 @@ struct ProxyGroupListView: View {
                             .environmentObject(viewModel)
                             .environmentObject(model)
                     } label: {
-                        HStack(spacing: 0) {
-                            VStack(alignment: .leading, spacing: 12) {
-                                Text(model.group.name)
-                                    .fontWeight(.medium)
-                                Text(model.group.type.uppercased())
-                                    .font(.system(size: 8))
-                                    .fontWeight(.bold)
-                                    .padding(.horizontal, 8)
-                                    .padding(.vertical, 4)
-                                    .background(Capsule().stroke(lineWidth: 1.0))
-                                    .foregroundColor(.accentColor)
-                                Text(model.isSelectable ? model.selectedProxy : "")
+                        GroupBox {
+                            HStack(spacing: 0) {
+                                VStack(alignment: .leading, spacing: 12) {
+                                    Text(model.group.name)
+                                        .fontWeight(.medium)
+                                    Text(model.group.type.uppercased())
+                                        .font(.system(size: 8))
+                                        .fontWeight(.bold)
+                                        .padding(.horizontal, 8)
+                                        .padding(.vertical, 4)
+                                        .background(Capsule().stroke(lineWidth: 1.0))
+                                        .foregroundColor(.accentColor)
+                                    Text(model.isSelectable ? model.selectedProxy : "")
+                                }
+                                .lineLimit(1)
+                                Spacer()
                             }
-                            .lineLimit(1)
-                            Spacer()
+                            .padding(8)
                         }
-                        .padding(8)
-                        .background(
-                            RoundedRectangle(cornerRadius: 6)
-                                .fill(colorScheme == .dark ? Color.black.opacity(0.3) : Color.white.opacity(0.7))
-                        )
                     }
                 }
             }
