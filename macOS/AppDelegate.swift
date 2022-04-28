32dd6b8322fc70079b279d4f7353fa6083d928c6
Arror <maqiang@autoai.com>
2022-04-28 17:16:19 +0800

    Apply theme when application launched

--- macOS/AppDelegate.swift
+++ macOS/AppDelegate.swift
@@ -2,9 +2,12 @@ import AppKit
 import SwiftUI
 
 final class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @AppStorage(Clash.theme) private var theme: Theme = .system
             
     func applicationDidFinishLaunching(_ notification: Notification) {
         NSWindow.allowsAutomaticWindowTabbing = false
+        theme.applyAppearance()
         GEOIPDatabaseManager.copyGEOIPDatabase()
     }
     
