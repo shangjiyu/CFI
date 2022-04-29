4f25ed3809bdbab10d279889aed78863089ac86e
Arror <maqiang@autoai.com>
2022-04-29 14:29:35 +0800

    Filter

--- macOS/Home/ConfigSwitchView.swift
+++ macOS/Home/ConfigSwitchView.swift
@@ -3,20 +3,16 @@ import SwiftUI
 struct ConfigSwitchView: View {
     
     @AppStorage(Clash.currentConfigUUID, store: .shared) private var uuidString: String = ""
-        
-    private var predicate: NSPredicate {
-        NSPredicate(format: "%K == %@", "uuid", (UUID(uuidString: self.uuidString) ?? UUID()).uuidString)
-    }
+    
+    @Environment(\.managedObjectContext) private var context
+    @FetchRequest(
+        sortDescriptors: [],
+        animation: nil
+    ) private var configs: FetchedResults<ClashConfig>
     
     var body: some View {
-        ManagedObjectFetchView(predicate: predicate) { (result: FetchedResults<ClashConfig>) in
-            label(result: result)
-        }
-    }
-    
-    private func label(result: FetchedResults<ClashConfig>) -> some View {
         HStack {
-            TextField("配置: ", text: .constant(result.first.flatMap({ $0.name ?? "-" }) ?? "未选择"), prompt: nil)
+            TextField("配置: ", text: .constant(configs.first(where: { $0.uuid?.uuidString == uuidString }).flatMap({ $0.name ?? "-" }) ?? "未选择"), prompt: nil)
                 .textFieldStyle(.plain)
                 .allowsHitTesting(false)
                 .focusable(false)
