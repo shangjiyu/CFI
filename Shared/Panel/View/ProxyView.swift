4d302f653b32ca836630b002f3ae1122016cac46
Arror <maqiang@autoai.com>
2022-05-06 17:44:35 +0800

    UI update

--- Shared/Panel/View/ProxyView.swift
+++ Shared/Panel/View/ProxyView.swift
@@ -12,18 +12,19 @@ struct ProxyView: View {
             HStack(spacing: 0) {
                 VStack(alignment: .leading, spacing: 8) {
                     HStack {
-                        Text(viewModel.type.uppercased())
-                            .font(.system(size: 8))
-                            .padding(.horizontal, 8)
-                            .padding(.vertical, 4)
-                            .background(RoundedRectangle(cornerRadius: 4).stroke(.tint))
-                        Spacer()
-                        Circle()
-                            .frame(width: 10, height: 10)
-                            .foregroundColor(selected == viewModel.name ? .green : .clear)
+                        HStack(alignment: .top, spacing: 0) {
+                            Text(viewModel.name)
+                            Text("\n")
+                            Spacer()
+                            if selected == viewModel.name {
+                                Circle()
+                                    .frame(width: 12, height: 12)
+                                    .foregroundColor(.green)
+                                    .padding(EdgeInsets(top: 2, leading: 8, bottom: 0, trailing: 0))
+                            }
+                        }
+                        .lineLimit(2)
                     }
-                    Text(viewModel.name)
-                        .foregroundColor(.secondary)
                     Text(viewModel.delay)
                         .font(.subheadline)
                         .foregroundColor(viewModel.delayTextColor)
