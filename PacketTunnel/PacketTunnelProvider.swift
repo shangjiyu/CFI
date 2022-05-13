020a58a7d164143968984fa623e8ebd1e36c7200
Arror <maqiang@autoai.com>
2022-05-13 09:10:28 +0800

    Update mask

--- PacketTunnel/PacketTunnelProvider.swift
+++ PacketTunnel/PacketTunnelProvider.swift
@@ -10,7 +10,7 @@ class PacketTunnelProvider: NEPacketTunnelProvider {
         let settings = NEPacketTunnelNetworkSettings(tunnelRemoteAddress: "254.1.1.1")
         settings.mtu = 1500
         settings.ipv4Settings = {
-            let settings = NEIPv4Settings(addresses: ["198.18.0.1"], subnetMasks: ["255.255.255.0"])
+            let settings = NEIPv4Settings(addresses: ["198.18.0.1"], subnetMasks: ["255.255.0.0"])
             settings.includedRoutes = [NEIPv4Route.default()]
             return settings
         }()
